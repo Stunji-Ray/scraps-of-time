0223a8be2d37bc9cc7d8ed665a9383c0b0b71aa3
Stunji Ray <36284062+Stunji-Ray@users.noreply.github.com>
2020-05-25 20:10:38 -0400

    Update scrapsoftime.py

--- scrapsoftime.py
+++ scrapsoftime.py
@@ -8,8 +8,8 @@ print(
 damage = 1
 health = 100
 map = []
-dangers = [0, 1, 0, 5, 0, 7, 1, 5, 1, 9, 2, 2, 2, 4, 2, 7, 3, 0, 3, 2, 3, 6, 3, 9, 4, 8, 5, 1, 5, 3, 5, 9, 6, 0, 6, 7,
-           7, 2, 7, 5, 7, 9, 8, 1, 8, 8, 9, 2, 9, 4, 9, 6, 9, 7]
+# dangers = [0, 1, 0, 5, 0, 7, 1, 5, 1, 9, 2, 2, 2, 4, 2, 7, 3, 0, 3, 2, 3, 6, 3, 9, 4, 8, 5, 1, 5, 3, 5, 9, 6, 0, 6, 7,
+#            7, 2, 7, 5, 7, 9, 8, 1, 8, 8, 9, 2, 9, 4, 9, 6, 9, 7]
 pos = [4, 5]
 v = 0
 for i in range(10):
@@ -31,27 +31,28 @@ while True:
         pos = [pos[0] + 1, pos[1]]
     if a == 4:
         pos = [pos[0] - 1, pos[1]]
-    for i in range(0, len(dangers), 2):
-        if pos[0] == dangers[i]:
-            if i + 1 < len(dangers):
-                if pos[1] == dangers[i + 1]:
-                    opp = random.randint(1, 10)
-                    opph = random.randint(1, 30)
-                    while True:
-                        health -= opp
-                        opph -= damage
-                        if health <= 0:
-                            print("You died")
-                            SyntaxError("you have died...")
-                        if opph <= 0:
-                            print("They died")
-                            damage += 3
-                            health += 5
-                            break
+    # for i in range(0, len(dangers), 2):
+    #     if pos[0] == dangers[i]:
+    #         if i + 1 < len(dangers):
+    #             if pos[1] == dangers[i + 1]:
+    if random.randint(0,5)==0:
+        opp = random.randint(1, 5)
+        opph = random.randint(1, 100)
+        while True:
+            health -= opp
+            opph -= damage
+            if health <= 0:
+                print("You died")
+                SyntaxError
+            if opph <= 0:
+                print("They died")
+                damage += 3
+                health += 5
+                break
     if pos == [0, 0] or pos == [0, 9] or pos == [9, 0] or pos == [9, 9]:
         v += 1
     if v == 4:
         print("you win")
         exit()
     if health<0:
-        SyntaxError("you died")
+        SyntaxError
